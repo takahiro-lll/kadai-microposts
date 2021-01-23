e8663fc01e43d1ba210125414809acc75b7d202d
takahiro <keepwellas@gmail.com>
2021-01-23 05:37:37 +0000

    timeline

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -69,4 +69,14 @@ class UsersController extends Controller
         ]);
     }
     
+    public function feed_microposts()
+    {
+        // このユーザがフォロー中のユーザのidを取得して配列にする
+        $userIds = $this->followings()->pluck('users.id')->toArray();
+        // このユーザのidもその配列に追加
+        $userIds[] = $this->id;
+        // それらのユーザが所有する投稿に絞り込む
+        return Micropost::whereIn('user_id', $userIds);
+    }
+    
 }
